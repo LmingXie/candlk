a96d61a6a9b194d2f37b5418a7f25f8be0e1ed46
Lming <lming1210@gmail.com>
2024-04-24 23:48:00 +0800

    完善：支持重试

--- candlk-server/condlk-user/src/main/java/com/candlk/webapp/job/XAIJob.java
+++ candlk-server/condlk-user/src/main/java/com/candlk/webapp/job/XAIJob.java
@@ -32,72 +32,80 @@ public class XAIJob {
 	@Scheduled(cron = "${service.cron.xai:0/5 * * * * ?}")
 	public void run() throws Exception {
 		final BigInteger lastBlock = web3j.ethBlockNumber().send().getBlockNumber();
+		int retry;
 		do {
 			final BigInteger blockNumber = web3JConfig.incrLastBlock();
-			SpringUtil.asyncRun(() -> {
-				final Web3j newWeb3j = web3JConfig.pollingGetWeb3j();
-				final EthBlock.Block block = newWeb3j.ethGetBlockByNumber(new DefaultBlockParameterNumber(blockNumber), true).send().getBlock();
-				log.info("正在执行扫描区块：{}", blockNumber);
-				final List<TransactionResult> txs = block.getTransactions();
-				if (!CollectionUtils.isEmpty(txs)) {
-					for (TransactionResult txR : txs) {
-						final TransactionObject tx = (TransactionObject) txR.get();
-						final Transaction info = tx.get();
-						final String from = info.getFrom(), to = info.getTo(), hash = info.getHash(), input = info.getInput(),
-								nickname = web3JConfig.spyFroms.get(from.toLowerCase()),
-								method = StringUtils.length(input) > 10 ? input.substring(0, 10) : null;
-						if (nickname != null) {
-							final String[] msg = METHOD2TIP.getOrDefault(method, METHOD2TIP.get(null))
-									.apply(new String[] { from, to, hash, input, nickname, method });
-							X.use(msg, m -> web3JConfig.sendWarn(msg[0], msg[1]));
-						}
-						// 只识别大额质押与赎回
-						else if ("0xf9e08660223e2dbb1c0b28c82942ab6b5e38b8e5".equalsIgnoreCase(to)) {
-							final Function<String[], String[]> function = METHOD2TIP.get(method);
-							if (function != null) {
-								final String[] msg = function.apply(new String[] { from, to, hash, input, from, method, "1" });
-								X.use(msg, m -> {
-									if (msg[1] != null) {
-										web3JConfig.sendWarn(msg[0], msg[1]);
-									} else {
-										log.info("识别到普通调用 -> {}：from={},to={},hash={}", msg[0], from, to, hash);
-									}
-								});
-							} else {
-								log.info("无法识别的调用：from={},to={},hash={}", from, to, hash);
-							}
-						}
-						// esXAI 合约监控
-						if ("0x4c749d097832de2fecc989ce18fdc5f1bd76700c".equalsIgnoreCase(to)) {
-							if ("0x840ecba0".equalsIgnoreCase(method)) { // 大额赎回的领取
-								SpringUtil.asyncRun(() -> {
-									final TransactionReceipt receipt = newWeb3j.ethGetTransactionReceipt(hash).send().getTransactionReceipt().get();
-									final List<Log> logs = receipt.getLogs();
-									if (logs.size() == 4) {
-										final Log redemption = logs.get(1), recycle = logs.get(2);
-										final BigDecimal redemptionAmount = new BigDecimal(new BigInteger(redemption.getData().substring(2), 16)).movePointLeft(18);
-										if (redemptionAmount.compareTo(web3JConfig.redemptionThreshold) >= 0) {
-											final BigDecimal totalAmount = new BigDecimal(new BigInteger(logs.get(0).getData().substring(2), 16)).movePointLeft(18),
-													recycleAmount = new BigDecimal(new BigInteger(recycle.getData().substring(2), 16)).movePointLeft(18)
-															.setScale(2, RoundingMode.HALF_UP);
-											final String redemptionTo = redemption.getTopics().get(2);
-											web3JConfig.sendWarn("预警：XAI大额赎回领取事件",
-													"### 预警：XAI大额赎回领取事件！  \n  "
-															+ "识别到【**" + this.getPeriod(redemptionAmount, totalAmount) + "**】天期限的赎回领取事件。  \n  "
-															+ "赎回数量：**" + redemptionAmount.setScale(2, RoundingMode.HALF_UP) + " XAI**  \n  "
-															+ "销毁数量：**" + recycleAmount.setScale(2, RoundingMode.HALF_UP) + " XAI**  \n  "
-															+ "赎回地址：**" + redemptionTo.replaceAll("0x000000000000000000000000", "0x") + "**  \n  "
-															+ "[点击前往查看详情](https://arbiscan.io/tx/" + hash + ")");
-										}
-									}
-								});
-							}
-						}
-					}
-				}
-			});
+			retry = 5;
+			while (retry-- > 0) {
+				SpringUtil.asyncRun(() -> {
+					final Web3j newWeb3j = web3JConfig.pollingGetWeb3j();
+					this.exec(newWeb3j, blockNumber);
+				}, "接口被限制，进行重试");
+			}
 		} while (lastBlock.compareTo(web3JConfig.lastBlock) > 0);
 		log.info("结束本次扫描，最后区块：{}", web3JConfig.lastBlock);
+	}
+
+	private void exec(Web3j newWeb3j, BigInteger blockNumber) throws Exception {
+		final EthBlock.Block block = newWeb3j.ethGetBlockByNumber(new DefaultBlockParameterNumber(blockNumber), true).send().getBlock();
+		log.info("正在执行扫描区块：{}", blockNumber);
+		final List<TransactionResult> txs = block.getTransactions();
+		if (!CollectionUtils.isEmpty(txs)) {
+			for (TransactionResult txR : txs) {
+				final TransactionObject tx = (TransactionObject) txR.get();
+				final Transaction info = tx.get();
+				final String from = info.getFrom(), to = info.getTo(), hash = info.getHash(), input = info.getInput(),
+						nickname = web3JConfig.spyFroms.get(from.toLowerCase()),
+						method = StringUtils.length(input) > 10 ? input.substring(0, 10) : null;
+				if (nickname != null) {
+					final String[] msg = METHOD2TIP.getOrDefault(method, METHOD2TIP.get(null))
+							.apply(new String[] { from, to, hash, input, nickname, method });
+					X.use(msg, m -> web3JConfig.sendWarn(msg[0], msg[1]));
+				}
+				// 只识别大额质押与赎回
+				else if ("0xf9e08660223e2dbb1c0b28c82942ab6b5e38b8e5".equalsIgnoreCase(to)) {
+					final Function<String[], String[]> function = METHOD2TIP.get(method);
+					if (function != null) {
+						final String[] msg = function.apply(new String[] { from, to, hash, input, from, method, "1" });
+						X.use(msg, m -> {
+							if (msg[1] != null) {
+								web3JConfig.sendWarn(msg[0], msg[1]);
+							} else {
+								log.info("识别到普通调用 -> {}：from={},to={},hash={}", msg[0], from, to, hash);
+							}
+						});
+					} else {
+						log.info("无法识别的调用：from={},to={},hash={}", from, to, hash);
+					}
+				}
+				// esXAI 合约监控
+				if ("0x4c749d097832de2fecc989ce18fdc5f1bd76700c".equalsIgnoreCase(to)) {
+					if ("0x840ecba0".equalsIgnoreCase(method)) { // 大额赎回的领取
+						SpringUtil.asyncRun(() -> {
+							final TransactionReceipt receipt = newWeb3j.ethGetTransactionReceipt(hash).send().getTransactionReceipt().get();
+							final List<Log> logs = receipt.getLogs();
+							if (logs.size() == 4) {
+								final Log redemption = logs.get(1), recycle = logs.get(2);
+								final BigDecimal redemptionAmount = new BigDecimal(new BigInteger(redemption.getData().substring(2), 16)).movePointLeft(18);
+								if (redemptionAmount.compareTo(web3JConfig.redemptionThreshold) >= 0) {
+									final BigDecimal totalAmount = new BigDecimal(new BigInteger(logs.get(0).getData().substring(2), 16)).movePointLeft(18),
+											recycleAmount = new BigDecimal(new BigInteger(recycle.getData().substring(2), 16)).movePointLeft(18)
+													.setScale(2, RoundingMode.HALF_UP);
+									final String redemptionTo = redemption.getTopics().get(2);
+									web3JConfig.sendWarn("预警：XAI大额赎回领取事件",
+											"### 预警：XAI大额赎回领取事件！  \n  "
+													+ "识别到【**" + this.getPeriod(redemptionAmount, totalAmount) + "**】天期限的赎回领取事件。  \n  "
+													+ "赎回数量：**" + redemptionAmount.setScale(2, RoundingMode.HALF_UP) + " XAI**  \n  "
+													+ "销毁数量：**" + recycleAmount.setScale(2, RoundingMode.HALF_UP) + " XAI**  \n  "
+													+ "赎回地址：**" + redemptionTo.replaceAll("0x000000000000000000000000", "0x") + "**  \n  "
+													+ "[点击前往查看详情](https://arbiscan.io/tx/" + hash + ")");
+								}
+							}
+						});
+					}
+				}
+			}
+		}
 	}
 
 	private final static BigDecimal PERIOD15 = new BigDecimal("0.25"), PERIOD90 = new BigDecimal("0.625"), PERIOD180 = BigDecimal.ONE;
