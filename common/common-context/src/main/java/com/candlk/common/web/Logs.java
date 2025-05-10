73a01f943836473807f27a36ccd1ca936046daa7
lming <lming@king.com>
2025-05-10 20:33:37 +0800

    删除：冗余代码2

--- common/common-context/src/main/java/com/candlk/common/web/Logs.java
+++ common/common-context/src/main/java/com/candlk/common/web/Logs.java
@@ -2,6 +2,7 @@ package com.candlk.common.web;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Consumer;
@@ -26,15 +27,12 @@ public class Logs {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(Logs.class);
 
-	public static void logJSON(@Nullable Long merchantId, @Nullable Long memberId, String appName, String requestMethod, String requestURI, JSONObject headersJson,
-	                           String params, String ip, long time, long useTimeMs, @Nullable Object retVal, @Nullable Throwable e) {
+	public static void logJSON(@Nullable Long memberId, String appName, String requestMethod, String requestURI, JSONObject headersJson,
+	                           String params, String ip, Date time, long useTimeMs, @Nullable Object retVal, @Nullable Throwable e) {
 
 		JSONObject json = new JSONObject();
 		json.put("app", appName);
-		if (memberId != null) {
-			json.put("mid", merchantId);
-			json.put("userId", memberId);
-		}
+		json.put("userId", memberId);
 		json.put("method", requestMethod);
 		json.put("uri", requestURI);
 		json.put("headers", headersJson);
