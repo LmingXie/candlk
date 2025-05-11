c08b2516c4ff7c9a486e93d3c63f5d451b4d7767
Lming <lming1210@gmail.com>
2025-05-11 11:14:25 +0800

    备份：SQL脚本

--- candlk-server/before.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `x_tweet_word`
-    ADD COLUMN `status` tinyint UNSIGNED NOT NULL DEFAULT 1 COMMENT '业务状态：0=未启用；1=已启用',
-    ADD COLUMN `provider_type` tinyint UNSIGNED NOT NULL DEFAULT 0 COMMENT '来源厂商类型';
