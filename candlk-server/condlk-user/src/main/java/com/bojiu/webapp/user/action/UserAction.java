a25479573deb301baec79aeb117f9cff0b359684
LmingXie <lming1210@gmail.com>
2025-11-26 12:30:37 +0800

    新增：加载接口

--- candlk-server/condlk-user/src/main/java/com/bojiu/webapp/user/action/UserAction.java
+++ candlk-server/condlk-user/src/main/java/com/bojiu/webapp/user/action/UserAction.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserAction extends BaseAction {
 	// TODO: 2025/11/18 导入
 	/*
-	1、py提供导入session+json账号接口，传递proxyId，仅解析来自Telegram的验证码消息（仅做临时存储，间隔10分钟检查并自动断开连接）
-	2、导入成功后，创建Client并通过 SetAuthenticationPhoneNumber 发送验证码
+	1、py提供导入账号接口，
+	2、导入成功后，创建Client并通过 SetAuthenticationPhoneNumber 发送验证码，记录发送时间
 	3、java定时查询正在导入的账号是否收到验证码，收到后通过 CheckAuthenticationCode、CheckAuthenticationPassword 进行验证录入
 	 */
+	// TODO: 2025/11/26 数据库迁移方案（考虑不进行迁移，java包存放到原有文件夹内，页面逻辑依旧只做静态路由）
 }
---
+++ candlk-server/condlk-user/src/main/java/com/bojiu/webapp/user/api/CockpitXApi.java
@@ -0,0 +1,84 @@
+package com.bojiu.webapp.user.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+import java.util.TreeMap;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.bojiu.common.model.Messager;
+import com.bojiu.common.util.BaseHttpUtil;
+import com.bojiu.context.web.Jsons;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+
+@Slf4j
+public class CockpitXApi extends BaseHttpUtil {
+
+	public static final String baseURI = "http://127.0.0.1:8985";
+	public static final URI loadUri = URI.create(baseURI + "/user/j/load"),
+			closeUri = URI.create(baseURI + "/user/j/close");
+
+	public static CockpitXApi getInstance() {
+		return new CockpitXApi();
+	}
+
+	final HttpClient proxyHttpClient;
+
+	public CockpitXApi() {
+		this.proxyHttpClient = defaultClient();
+	}
+
+	/**
+	 * @return 返回的 data 属性 是 JSONObject，返回的 ext 响应的原始文本
+	 */
+	public Messager<JSONObject> doRequest(HttpMethod method, URI uri, String body) {
+		HttpRequest.Builder builder = requestBuilder(method, uri, body, true);
+		String responseBody = null;
+		Exception ex = null;
+		try {
+			HttpResponse<String> response = doSend(this.proxyHttpClient, builder.build(), responseBodyHandler);
+			responseBody = response.body();
+			if (response.statusCode() == 200) {
+				final JSONObject json = Jsons.parseObject(responseBody);
+				return Messager.hideData(json).setExt(responseBody);
+			}
+			return new Messager<JSONObject>().setExt(responseBody);
+		} catch (IOException | IllegalThreadStateException e) {
+			ex = e;
+			return Messager.status(Messager.ERROR);
+		} finally {
+			if (ex == null) {
+				log.info("【CockpitX】请求地址：{}\n请求参数：{}\n返回数据：{}", uri, body, responseBody);
+			} else {
+				log.error("【CockpitX】请求地址：" + uri + "\n请求参数：" + body, ex);
+			}
+		}
+	}
+
+	/**
+	 * @return 返回的 data 属性 是 JSONObject，返回的 ext 响应的原始文本
+	 */
+	public Messager<JSONObject> doGet(URI uri) {
+		return doRequest(HttpMethod.GET, uri, null);
+	}
+
+	public Messager<String> load(Long id) {
+		return sendReq(id, loadUri);
+	}
+
+	public Messager<String> close(Long id) {
+		return sendReq(id, closeUri);
+	}
+
+	private Messager<String> sendReq(Long id, URI uri) {
+		TreeMap<String, Object> params = new TreeMap<>();
+		params.put("id", id);
+		Messager<JSONObject> resp = doRequest(HttpMethod.POST, uri, Jsons.encodeRaw(params));
+		if (resp.isOK()) {
+			return resp.castDataType(null);
+		}
+		return null;
+	}
+
+}
