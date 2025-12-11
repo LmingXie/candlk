c0f01e87d518ce58a213a8659429ec7f1e2a0e88
LmingXie <lming1210@gmail.com>
2025-12-11 14:31:44 +0800

    完善：预估对冲投注算法

--- candlk-server/condlk-user/src/main/java/com/bojiu/webapp/user/dto/HedgingDTO.java
+++ candlk-server/condlk-user/src/main/java/com/bojiu/webapp/user/dto/HedgingDTO.java
@@ -16,12 +16,12 @@ public class HedgingDTO {
 		this.parlays = parlays;
 	}
 
-	/** A平台本金 */
+	/** A平台 本金 */
 	public double aPrincipal = 1000,
 	/** A平台充值返奖（%） */
 	aRechargeRate = 0,
 	/** 投注返水比例（%） */
-	aRebate = 0.02,
+	aRebate = 0.015,
 	/** B平台投注返水比例（%） */
 	bRebate = 0.025;
 
@@ -45,23 +45,23 @@ public class HedgingDTO {
 
 		transient Double bWinFactor;
 
-		/** B平台赢的净收益系数 */
+		/** B平台 赢的净收益系数 */
 		public double bWinFactor(double bRebate) {
-			return bWinFactor == null ? bWinFactor = bRate - 1 + bRebate : bWinFactor;
+			return bWinFactor == null ? bWinFactor = (bRate - 1) * (1 + bRebate) : bWinFactor;
 		}
 
 	}
 
 	transient Double bLossFactor;
 
-	/** B平台输的净收益系数 */
+	/** B平台 输的净收益系数 */
 	public double bLossFactor() {
 		return bLossFactor == null ? bLossFactor = bRebate - 1 : bLossFactor;
 	}
 
-	/** 综合赔率 */
 	transient Double overallOdds;
 
+	/** 综合赔率 */
 	public double overallOdds() {
 		if (overallOdds == null) {
 			double odds = parlays[0].aRate;
@@ -75,35 +75,36 @@ public class HedgingDTO {
 
 	transient Double aInCoin;
 
-	/** A平台串子投注金额 */
+	/** A平台 串子投注金额 */
 	public double getAInCoin() {
 		return aInCoin == null ? aInCoin = aPrincipal * (1 + aRechargeRate) : aInCoin;
 	}
 
 	transient Double aRebateCoin;
 
-	/** A平台返水金额 */
+	/** A平台 返水金额（串子全输时） */
 	public double getARebateCoin() {
 		return aRebateCoin == null ? aRebateCoin = getAInCoin() * aRebate : aRebateCoin;
 	}
 
 	transient Double loss;
 
-	/** A平台串关输时的固定收益 */
+	/** A平台 串关全输时的固定收益 */
 	public double getLoss() {
 		return loss == null ? loss = getARebateCoin() - aPrincipal : loss;
 	}
 
 	transient Double win;
 
-	/** A平台串关赢时的固定收益 */
+	/** A平台 串关全赢时的固定收益（按输赢金额计算返水） */
 	public double getWin() {
-		return win == null ? win = getAInCoin() * overallOdds() + getARebateCoin() - aPrincipal : win;
+		final double odds = overallOdds();
+		return win == null ? win = (getAInCoin() * odds) + (getARebateCoin() * Math.abs(odds - 1)) - aPrincipal : win;
 	}
 
 	transient double[] hedgingCoins;
 
-	/** 根据当前的赔率估算在B平台对冲的每场投注金额 */
+	/** 根据当前的赔率估算在 B平台 对冲的每场投注金额 */
 	public double[] getHedgingCoins() {
 		if (hedgingCoins == null) {
 			final int size = parlays.length;
@@ -132,38 +133,49 @@ public class HedgingDTO {
 
 	}
 
+	transient List<Out> outs;
+
 	/** 计算三串一利润 */
 	public List<Out> calcProfit(double[] bHedgingCoins) {
-		List<Out> outs = new ArrayList<>(4);
-		final int N = parlays.length;
-		// 初始成本和产出
-		double aInCoin = getAInCoin(), sumInCoin = aInCoin, sumRebate = getARebateCoin();
+		if (outs == null) {
+			final int N = parlays.length;
+			final List<Out> result = new ArrayList<>(N + 1);
+			// 初始成本和产出
+			double loss_ = getLoss(), bLossFactor_ = bLossFactor(), sumBInCoin = 0;
+			// 计算 A平台 串子输的情况
+			for (int i = 0; i < N; i++) {
+				double bHedgingCoin = bHedgingCoins[i];
 
-		// 计算A平台串子输的情况
-		for (int i = 0; i < N; i++) {
-			double bHedgingCoin = bHedgingCoins[i];
-			sumInCoin += bHedgingCoin;
-			sumRebate += bHedgingCoin * bRebate;
-
-			outs.add(new Out("串子在M" + (i + 1) + "结束", (bHedgingCoin * parlays[i].bRate) + sumRebate - sumInCoin));
+				result.add(new Out("串子第" + (i + 1) + "场输", loss_ + (bLossFactor_ * sumBInCoin) + (bHedgingCoin * parlays[i].bWinFactor(bRebate))));
+				sumBInCoin += bHedgingCoin;
+			}
+			// 计算 A平台 串子赢的情况
+			result.add(new Out("串子全中", getWin() + (bLossFactor_ * sumBInCoin)));
+			outs = result;
 		}
+		return outs;
+	}
 
-		// 计算A平台串子赢的情况
-		outs.add(new Out("串子全中", (aInCoin * overallOdds()) + sumRebate - sumInCoin));
-
-		// 3. 盈利排名 (按最大净收益从高到低排序)
+	public List<Out> getOuts() {
+		List<Out> outs = calcProfit(hedgingCoins);
+		// 盈利排名 (按最大净收益从高到低排序)
 		outs.sort(Comparator.comparingDouble(r -> r.out));
 		Collections.reverse(outs);
-
 		return outs;
 	}
 
 	public static void main(String[] args) {
 		HedgingDTO dto = new HedgingDTO(new Odds[] {
-				new Odds(1.95, 2),
-				new Odds(2, 1.98),
-				new Odds(1.95, 1.96)
+				new Odds(2.06, 1.85),
+				new Odds(2.08, 1.9),
+				new Odds(2, 1.82)
 		});
+		System.out.println("综合赔率：" + dto.overallOdds());
+		System.out.println("A平台 串子投注金额：" + dto.getAInCoin());
+		System.out.println("A平台 返水金额（串子全输时）：" + dto.getARebateCoin());
+		System.out.println("A平台 串关全输时的固定收益：" + dto.getLoss());
+		System.out.println("A平台 串关全赢时的固定收益（按输赢金额计算返水）：" + dto.getWin());
+
 		double[] hedgingCoins1 = dto.getHedgingCoins();
 		System.out.println("最佳对冲下注方案：" + Jsons.encode(hedgingCoins1));
 		List<Out> profitResults = dto.calcProfit(hedgingCoins1);
